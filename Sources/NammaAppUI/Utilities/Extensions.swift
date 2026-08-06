964ac80c9e4ba3bbd7e0195c02b7746618bedca0
Anand <anand.simplextech@gmail.com>
2026-08-06 19:04:50 +0530

    refactor(Access Specifier): created extension for converting hex code to swift Color and made it public

--- Sources/NammaAppUI/Utilities/Extensions.swift
+++ Sources/NammaAppUI/Utilities/Extensions.swift
@@ -8,7 +8,7 @@
 import SwiftUI
 
 extension Color {
-    init(hex: Int, alpha: Double = 1) {
+    public init(hex: Int, alpha: Double = 1) {
         let components = (
             R: Double((hex >> 16) & 0xff) / 255,
             G: Double((hex >> 08) & 0xff) / 255,
